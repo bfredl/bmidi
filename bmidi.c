9305d9f2a4327277eca90774b3e0816500eb94ca
bfredl <bjorn.linse@gmail.com>
2023-06-08 22:13:24 +0200

    pika

--- bmidi.c
+++ bmidi.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv)
 {
-  if (argc < 2) return 1;
+  if (argc < 4) return 1;
   char *port_name = argv[1];
   static snd_rawmidi_t *input;
   static snd_rawmidi_t *output;
@@ -15,10 +15,28 @@ int main(int argc, char **argv)
     return 2;
   }
 
-  //char data[] = {0xf0, 'a', 'b', 'a', 'c', 'u', 's', 0xf7};
-  char data[] = "\xf0hello sysex\xf7";
+  int cmd, pos, x = 0;
+  sscanf(argv[2], "%d", &cmd);
+  sscanf(argv[3], "%d", &pos);
+  if (argc >= 5) sscanf(argv[4], "%d", &x);
 
-  status = snd_rawmidi_write(output, data, sizeof(data));
+  cmd = cmd & 0x7f;
+  int pos_low = pos & 0x7f;
+  int pos_high = (pos >> 7) & 0x7f;
+  x = x & 0x7f;
+  printf("cmd: %d, the split: %d %d, x %d\n", cmd, pos_low, pos_high, x);
+
+  //char data[] = {0xf0, 'a', 'b', 'a', 'c', 'u', 's', 0xf7};
+  uint8_t data[17] = "\xf0hello sysex\xf7";
+  data[0] = 0xf0;
+  data[1]= 0x67;
+  data[2] = cmd;
+  data[3] = pos_low;
+  data[4] = pos_high;
+  data[5] = x;
+  data[6] = 0xf7;
+
+  status = snd_rawmidi_write(output, data, 7);
   if (status < 0) {
     fprintf(stderr, "no write! %s", snd_strerror(status));
   }
