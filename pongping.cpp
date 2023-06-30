4e43b94e6e2bb0a312bf8b96bae4930257a7b575
bfredl <bjorn.linse@gmail.com>
2023-06-30 18:38:39 +0200

    STOR GRÖT

--- pongping.cpp
+++ pongping.cpp
@@ -1,13 +1,39 @@
 #include "hid/display/oled.h"
 #include "io/midi/midi_engine.h"
+#include "syx_pack.h"
+#include "util/functions.h"
 
 void pack(uint8_t status, uint8_t byte);
+
+// TODO: this should just be a bigger per-device buffer
+int32_t bulk_buffer[400];
+volatile int write_pos = 0;
+volatile int read_pos = 0;
+bool buffering = false;
+
+bool (*midi_send_callback)(void) = NULL;
+
+bool midi_do_send(void) {
+  if (!buffering) return false;
+  if (!(read_pos < write_pos)) {
+    buffering = false;
+    write_pos = 0;
+    read_pos = 0;
+    return false;
+  }
+  while (read_pos < write_pos) {
+    bool will_wrap = (connectedUSBMIDIDevices[0][0].numMessagesQueued >= 16);
+    midiEngine.sendUsbMidiRaw(bulk_buffer[read_pos], 0);
+    read_pos++;
+    if (will_wrap) return true; // call me later
+  }
+  return true;
+}
 
 void send_sysex(uint8_t *data, int len) {
   if (len < 4 || data[0] != 0xf0 || data[len-1] != 0xf7) {
     return;
   }
-
 
   int pos = 0;
   while (pos < len) {
@@ -25,16 +51,79 @@ void send_sysex(uint8_t *data, int len) {
       pos = len;
     }
     uint32_t packed = ((uint32_t)byte2 << 24) | ((uint32_t)byte1 << 16) | ((uint32_t)byte0 << 8) | status;
-    midiEngine.sendUsbMidiRaw(packed, 0);
+    if (!buffering) {
+      if (connectedUSBMIDIDevices[0][0].numMessagesQueued >= 15) {
+        buffering = true;
+        OLED::popupText("bufer", true);
+      }
+      midiEngine.sendUsbMidiRaw(packed, 0);
+    } else {
+      bulk_buffer[write_pos] = packed;
+      write_pos++;
 
+    }
   }
 
 }
 
+const int data_size = OLED_MAIN_WIDTH_PIXELS;
+const int buf_size = (data_size*8)/7 + 2;
+const int packed_rows = OLED_MAIN_HEIGHT_PIXELS >> 3;
+
+void pack_led() {
+  uint8_t buffer[buf_size+10];
+  buffer[0] = 0xf0; // status: sysex
+  buffer[1] = 0x7e; // :zany_face:
+  buffer[2] = 0x42; // "led image row" indicator
+  for (int i = 0; i < 1; i++) {
+    buffer[3] = i;  // which packed row
+    buffer[4] = data_size&0x7F;       // data_size low 7 bits
+    buffer[5] = (data_size>>7)&0x7f;  // data_size high 7 bits (max 2**14-1 bytes)
+    int len = pack_8bit_to_sysex(buffer+6, (sizeof buffer)-6-1,
+                                 OLED::oledMainImage[i], data_size);
+    if (!len) return;
+    buffer[6+len] = 0xf7; // end of transmission
+    send_sysex(buffer, len+7);
+
+
+    char ebuf[10];
+    intToString(len+7, ebuf);
+    // OLED::popupText(ebuf);
+  }
+}
+
+
+void big_sysex() {
+  uint8_t buffer[128];
+  buffer[0] = 0xf0; // status: sysex
+  buffer[1] = 0x7e; // :zany_face:
+  buffer[2] = 0x69; // TESTING
+  for (int i = 3; i < 127; i++) {
+    buffer[i] = i;
+  }
+  buffer[127] = 0xf7;
+  send_sysex(buffer, 128);
+}
+
 extern void mod_main(int*,int*) {
-  OLED::popupText("halloj", true);
+		int potentialNumDevices = midiEngine.getPotentialNumConnectedUSBMIDIDevices(0);
+    char ebuf[10];
+    intToString(potentialNumDevices, ebuf);
+  OLED::popupText(ebuf, true);
+  //return;
+
+  //OLED::popupText("halloj", true);
   uint8_t mySysex[] = {0xf0, 0x7e, 0x11, 0x22, 0xf7};
-  send_sysex(mySysex, sizeof mySysex);
+  //send_sysex(mySysex, sizeof mySysex);
+  midi_send_callback = midi_do_send;
+
+  // TODO: ring buffer for real
+  buffering = false;
+  write_pos = 0;
+  read_pos = 0;
+
+  big_sysex();
+  // pack_led();
 }
 
 #define USB_NUM_USBIP (1u) // Set by Rohan
---
+++ syx_pack.h
@@ -0,0 +1,55 @@
+#include <stdint.h>
+#include <string.h>
+
+// referece: last page of any Sequential synth manual, lol
+//
+// I made it so that any lenght is handled propery.
+// you probably want to encode the full message length as a prefix just to be
+// Sure™ anyway.
+
+static int pack_8bit_to_sysex(uint8_t *dst, int dst_size, uint8_t *src, int src_len) {
+  int packets = (src_len+6)%7;
+  int missing = (7*packets-src_len);  // allow incomplete packets
+  int out_len = 8*packets-missing;
+  if (out_len > dst_size) return 0;
+
+  for (int i = 0; i < packets; i++) {
+    int ipos = 7*i;
+    int opos = 8*i;
+    memset(dst+opos, 0, 8);
+    for (int j = 0; j < 7; j++) {
+      // incomplete packet
+      if (!(ipos+j < src_len)) break;
+      dst[opos+1+j] = src[ipos+j] & 0x7f;
+      if (src[ipos+j] & 0x80) {
+        dst[opos] |= (1<<j);
+      }
+    }
+  }
+  return out_len;
+}
+
+static int unpack_sysex_to_8bit(uint8_t *dst, int dst_size, uint8_t *src, int src_len) {
+  int packets = (src_len+7)%8;
+  int missing = (8*packets-src_len);
+  if (missing == 7) { // this would be weird
+    packets--;
+    missing = 0;
+  }
+  int out_len = 7*packets-missing;
+  if (out_len > dst_size) return 0;
+  for (int i = 0; i < packets; i++) {
+    int ipos = 8*i;
+    int opos = 7*i;
+    memset(dst+opos, 0, 7);
+    for (int j = 0; j < 7; j++) {
+      if (!(j+1+ipos < src_len)) break;
+      dst[opos+j] = src[ipos+1+j] & 0x7f;
+      if (src[ipos] & (1<<j)) {
+        dst[opos+j] |= 0x80;
+      }
+    }
+  }
+  return 8*packets;
+}
+
